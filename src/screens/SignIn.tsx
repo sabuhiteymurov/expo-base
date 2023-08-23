d6528f9de7680164223886516050ec6456bf325c
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2023-08-24 00:14:08 +0400

    feat: preserve user session

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { useRouter } from 'expo-router';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { storage } from '../helpers/storage';
 
 const SignIn = () => {
+  const router = useRouter();
+
+  const navigateToDashboard = () => {
+    // Important note: Currently, React Native MMKV is not supported in Expo Go.
+    // Instead, consider using Expo Dev Build.
+    // Alternatively, you can find a workaround by using react-native-async-storage.
+    storage.set('user', JSON.stringify({ name: 'John Doe', age: 23 }));
+    router.push('/home');
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View style={styles.container}>
+      <Text>Sign In screen</Text>
+      <Button title='Go to dashboard' onPress={navigateToDashboard} />
     </View>
   );
 };
 
-export default SignIn;
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    gap: 20,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default SignIn;
