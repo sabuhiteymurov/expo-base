a12f2c2aee0f446dcdc0d654c607591754ea8b42
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2024-04-27 14:57:15 +0400

    fix: runtime errors

--- src/context/ctx.tsx
+++ src/context/ctx.tsx
@@ -1,36 +1,31 @@
-import React from 'react';
+import { createContext, useContext } from 'react';
 import { useStorageState } from '../hooks/useStorageState';
 
 export interface Session {
-  signIn: (token: string) => void;
+  signIn: (tokens: string) => void;
   signOut: () => void;
   session?: string | null;
   isLoading: boolean;
 }
 
-const AuthContext = React.createContext<Session | null>(null);
+const AuthContext = createContext<Session | null>(null);
 
 // This hook can be used to access the user info.
 export function useSession() {
-  const value = React.useContext(AuthContext);
-  if (process.env.NODE_ENV !== 'production') {
-    if (!value) {
-      throw new Error('useSession must be wrapped in a <SessionProvider />');
-    }
-  }
+  const value = useContext(AuthContext);
 
   return value as Session;
 }
 
 export function SessionProvider(props: any) {
-  const [[isLoading, session], setSession] = useStorageState('token');
+  const [[isLoading, session], setSession] = useStorageState('session');
 
   return (
     <AuthContext.Provider
       value={{
-        signIn: async (token: string) => {
+        signIn: async (tokens: string) => {
           // Perform sign-in logic here
-          setSession(token);
+          setSession(JSON.stringify(tokens));
         },
         signOut: () => {
           setSession(null);
